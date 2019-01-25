8b58f41532994b881133fc1a8afb931bd57fe991
Frank <frank@iMac3.local>
2019-01-25 10:47:01 -0500

    bricks

---
+++ venv/brick.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+class Brick:
+
+    def __init__(self, main_surface, width, height, color):
+        self.main_surface = main_surface
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self, x, y):
+        pygame.draw.rect(self.main_surface, self.color, (x, y, self.width, self.height), 0)
+        pygame.display.update()
